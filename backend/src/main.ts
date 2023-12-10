c67a2254dabb2ddb16b828f751eb0754e104668c
arya020595 <arya.rifqi@alodokter.com>
2023-12-10 15:21:39 +0700

    update config swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,17 +4,16 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api');
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('API Lviors')
+    .setDescription('The lvior API')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('lviors')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
-
-  app.enableCors();
-  app.setGlobalPrefix('api');
 
   await app.listen(3000);
 }
