2740bfd4336c3eb74c66e5933ca16ea08e63ba48
arya020595 <arya.rifqi@alodokter.com>
2024-03-31 23:39:23 +0700

    update host for deployment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -22,7 +22,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('', app, document);
 
-  await app.listen(3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port, '0.0.0.0');
 
   const server = app.getHttpServer();
   const router = server._events.request._router;
